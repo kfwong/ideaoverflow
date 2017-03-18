55c727e4334382850d44b8a5c63151965f97a9b8
Kai Lin <kailin.t@gmail.com>
2017-03-18 10:53:36 +0800

    Navbar is fixed on top

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -14,7 +14,44 @@
 
 @section('content')
 <div class="posts">
-	
+	<div>
+		<div>
+			<h3>Post title <small><span class="label label-success">Idea</span></small></h3>
+			<h5><strong>Kang Fei</strong> @kfwong</h5>
+		</div>
+	</div>
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae perferendis sapiente provident modi, commodi itaque eaque vitae ut tempora fugit quidem facere animi iste, officia numquam maiores eum veniam accusantium!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur aspernatur itaque atque ab, quo eius adipisci distinctio quibusdam ullam dicta et nulla, explicabo totam dolorum fugiat perspiciatis, optio praesentium dolorem. <a href="">(More)</a></p>
+	</div>
+	<div>
+		<ul class="list-inline">
+			<li><a href="">Like</a></li>
+			<li><a href="">Comments</a> <span class="badge">5</span></li>
+		</ul>
+		
+	</div> 
+</div> <!-- posts -->
+<div class="posts">
+	<div>
+		<div>
+			<h3>Post title <small><span class="label label-success">Idea</span></small></h3>
+			<h5><strong>Kang Fei</strong> @kfwong</h5>
+		</div>
+	</div>
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae perferendis sapiente provident modi, commodi itaque eaque vitae ut tempora fugit quidem facere animi iste, officia numquam maiores eum veniam accusantium!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur aspernatur itaque atque ab, quo eius adipisci distinctio quibusdam ullam dicta et nulla, explicabo totam dolorum fugiat perspiciatis, optio praesentium dolorem. <a href="">(More)</a></p>
+	</div>
+	<div>
+		<ul class="list-inline">
+			<li><a href="">Like</a></li>
+			<li><a href="">Comments</a> <span class="badge">5</span></li>
+		</ul>
+		
+	</div> 
+</div> <!-- posts -->
+<div class="posts">
 	<div>
 		<div>
 			<h3>Post title <small><span class="label label-success">Idea</span></small></h3>
