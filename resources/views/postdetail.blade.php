d5b8e2e2b01b4fbe946b23ae4df86ed7120a6b7b
Kai Lin <kailin.t@gmail.com>
2017-03-19 23:58:04 +0800

    Added comment form to detailed view

--- resources/views/postdetail.blade.php
+++ resources/views/postdetail.blade.php
@@ -36,19 +36,28 @@
 <div>
 	<h3>{{'Comments ('.$post->comments_count.')'}}</h3>
 	@if($post->comments_count > 0)
-		@foreach($post->comments as $comment) 
-			<div class="comment">
-				<div>
-				<h5><a class="user-name" href="{{ 'users/'.$comment->user->id }}">{{$comment->user->name}}</a> {{ '@'.$comment->user->username }}</h5>
-				</div>
-				<div>
-					<p>{{$comment->description}}</p>
-				</div>
-			</div>
-		@endforeach
+	@foreach($post->comments as $comment) 
+	<div class="comment">
+		<div>
+			<h5><a class="user-name" href="{{ 'users/'.$comment->user->id }}">{{$comment->user->name}}</a> {{ '@'.$comment->user->username }}</h5>
+		</div>
+		<div>
+			<p>{{$comment->description}}</p>
+		</div>
+	</div>
+	@endforeach
 	@else
-		<p>No comments</p>
+	<p>No comments</p>
 	@endif
+</div>
+
+<div id="form-comment-container">
+	{{ Form::open(['class' => 'form-inline']) }}
+	<div class="form-group">
+	{{ Form::textarea('content', 'Write a comment...', ['class' => 'form-control', 'rows' => '3', 'id' => 'form-comment']) }}
+		{{ Form::submit('Submit', ['class'=>'btn btn-default', 'id' => 'form-comment-submit']) }}
+	</div>
+	{{ Form::close() }}
 </div>
 
 @endsection
