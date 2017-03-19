0ddf0f27579824da1489ee3c509faaa718105dc2
Anna He <jxh604@case.edu>
2017-03-20 00:36:53 +0800

    add like button to footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,4 +2,15 @@
 <footer class="center">
     Copyright @ 2017 Ideaoverflow
 
+<br>
+
+<!--TODO: @annahe change IP address -->
+  <div class="fb-like" 
+    data-href="188.166.214.134"  
+    data-layout="button_count" 
+    data-action="like" 
+    data-size="small" 
+    data-show-faces="true" 
+    data-share="true"></div>
+    
 </footer>
