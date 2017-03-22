e5b204a8efa344f2319bd52e870089d550c2c0a5
kfwong <kfwong@zoho.com>
2017-03-22 19:36:21 +0800

    Added gulp task. Change the title of post to proper sentence. Added dark theme :O. Some draft styling.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue-2');
+
+
+elixir(function (mix) {
+
+    mix.scripts([
+        'main.js'
+    ]);
+
+    mix.copy('resources/assets/img', 'public/img');
+
+    mix.copy('resources/assets/css', 'public/css')
+
+    mix.copy('resources/assets/js/', 'public/js/');
+});
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,38 +5,78 @@
 @section('content-title', 'Ideaoverflow')
 
 @section('stylesheet')
-{{ Html::style('css/posts.css') }}
+    {{ Html::style('css/posts.css') }}
+    <style>
+        /* Base styles */
+        .column {
+            float: left;
+        }
+        .size-1of4 {
+            width: 25%;
+            padding: 10px;
+        }
+        .size-1of3 {
+            width: 33.333%;
+            padding: 10px;
+        }
+        .size-1of2 {
+            width: 50%;
+            padding: 10px;
+        }
+
+        /* Configurate salvattore with media queries */
+        @media screen and (max-width: 450px) {
+            #posts[data-columns]::before {
+                content: '1 .column';
+            }
+        }
+
+        @media screen and (min-width: 451px) and (max-width: 700px) {
+            #posts[data-columns]::before {
+                content: '2 .column.size-1of2';
+            }
+        }
+
+        @media screen and (min-width: 701px) {
+            #posts[data-columns]::before {
+                content: '3 .column.size-1of3';
+            }
+        }
+    </style>
 @endsection
 
 @section('script')
-
+    {{ Html::script('https://cdnjs.cloudflare.com/ajax/libs/salvattore/1.0.9/salvattore.min.js') }}
 @endsection
 
 @section('content')
 
 @if(isset($posts))
-@foreach($posts as $post)
+    <div id="posts" data-columns>
+        @foreach($posts as $post)
 
-<div class="posts">
-	<div>
-		<div>
-			<h3><a class="post-title" href="{{'/posts/'.$post->id}}">{{ $post->title }}</a> <small><a href="" class="{{'post-tag '.$post->tag.'-tag'}}">{{ ucfirst($post->tag) }}</a></small></h3>
-			<h5><a class="user-name" href="{{ '/users/'.$post->user['id'] }}">{{$post->user['name']}}</a> {{ '@'.$post->user['username'] }}</h5>
-		</div>
-	</div>
-	<div>
-		<p>{{ $post->body }}</p>
-	</div>
-	<div>
-		<ul class="list-inline">
-			<li><button class="btn btn-default"><span class="glyphicon glyphicon-thumbs-up"></span>{{' Like | 4'}}</button></li>
-			<li><a href="{{'/posts/'.$post->id}}">{{ $post->comments_count . ' Comment' . ($post->comments_count > 1? 's' : '')}}</a></li>
-		</ul>
-		
-	</div> 
-</div> <!-- posts -->
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="modal-title">
+                        <a class="post-title" href="{{'/posts/'.$post->id}}">{{ $post->title }}</a>
+                        <small>
+                            <a href="" class="{{'post-tag '.$post->tag.'-tag'}}">{{ ucfirst($post->tag) }}</a>
+                        </small>
+                    </h4>
+                    <a class="user-name" href="{{ '/users/'.$post->user['id'] }}">{{ $post->user['username'] }} @ {{$post->created_at->diffForHumans()}}</a>
+                </div>
+                <div class="panel-body">
+                    <p>{{ $post->body }}</p>
+                </div>
 
-@endforeach
+                <div class="panel-footer">
+                    <button class="btn btn-default btn-sm"><span class="fa fa-thumbs-up"></span>{{' Like | 199'}}</button>
+                    <small class="pull-right" style="padding: 8px 0px 8px 0px"><a href="{{'/posts/'.$post->id}}">{{ $post->comments_count . ' Comment' . ($post->comments_count > 1? 's' : '')}}</a></small>
+                </div>
+            </div>
+
+        @endforeach
+    </div>
 @endif
 
 @endsection
