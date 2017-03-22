158f2291cfd47bf72e45040d5997e9cffa4777cd
kfwong <kfwong@zoho.com>
2017-03-22 20:27:04 +0800

    Added styling to postdetail page.

--- resources/views/postdetail.blade.php
+++ resources/views/postdetail.blade.php
@@ -22,21 +22,11 @@
 @endif
 
 <div class="posts">
-	<div>
-		<div>
-			<h3>{{ $post->title }} <small><a href="" class=""></a></small></h3>
-			<h5><a class="user-name" href="{{ '/users/'.$post->user->id }}">{{ucfirst($post->user->name)}}</a> {{ '@'.$post->user->username }}</h5>
-		</div>
-	</div>
-	<div>
-		<p>{{ $post->body }}</p>
-	</div>
-	<div>
-		<ul class="list-inline">
-			<li><button class="btn btn-default"><span class="glyphicon glyphicon-thumbs-up"></span>{{' Like | 10'}}</button></li>
-		</ul>
-		
-	</div> 
+    <div class="jumbotron">
+        <h1>{{ $post->title }}</h1>
+        <h5><a class="user-name" href="{{ '/users/'.$post->user->id }}">{{ucfirst($post->user->name)}}</a> {{ '@'.$post->user->username }}</h5>
+        <p>{{ $post->body }}</p>
+    </div>
 </div> <!-- posts -->
 
 <div>
@@ -45,29 +35,42 @@
         <p>No comments</p>
     @else
         @foreach($post->comments as $comment)
-        <div class="comment comment-{{$comment->id}}">
-            <div>
-                <h5><a class="user-name" href="{{ '/users/'.$comment->user->id }}">{{ucfirst($comment->user->name)}}</a> {{ '@'.$comment->user->username }}</h5>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="media comment-{{$comment->id}}">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object img-circle" src="https://placehold.it/64x64" alt="profile-pic">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">
+                                <p><a class="user-name" href="{{ '/users/'.$comment->user->id }}">{{ $comment->user->username }}</a></p>
+                                <p><small>{{$comment->created_at->diffForHumans()}}</small></p>
+                            </h4>
+                            <p>{{$comment->body}}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <p>{{$comment->body}}</p>
-            </div>
-        </div>
         @endforeach
     @endif
 </div>
-
-<div id="form-comment-container">
-	@can('create', App\Comment::class)
-        <h5><span class="user-name">{{ ucfirst(Auth::user()->name) }}</span>{{ ' @'.Auth::user()->username }}</h5>
-
-        {{ Form::open(['class' => 'form-inline', 'action' => ['CommentController@store', $post]]) }}
-        <div class="form-group">
+@can('create', App\Comment::class)
+    <div class="panel panel-default">
+        {{ Form::open(['class' => 'form-horizontal', 'action' => ['CommentController@store', $post]]) }}
+        <div class="panel-body">
+            <h5><span class="user-name">{{ ucfirst(Auth::user()->name) }}</span>{{ ' @'.Auth::user()->username }}</h5>
             {{ Form::textarea('body', '', ['class' => 'form-control', 'rows' => '3', 'id' => 'form-comment', 'placeholder' => 'Write a comment...']) }}
+        </div>
+        <div class="panel-footer">
             {{ Form::submit('Submit', ['class'=>'btn btn-default', 'id' => 'form-comment-submit']) }}
         </div>
+
         {{ Form::close() }}
-	@else
-        <p><a href="/login">Login</a> to post your comment</p>
-	@endif
-</div>
+    </div>
+@else
+    <p><a href="/login">Login</a> to post your comment</p>
+@endif
+
+@endsection
