840f5c7a5412e27e67643d1238a2005743c2f633
Fang Ruoming <frm10802@hotmail.com>
2017-03-27 03:48:23 +0800

    implementation of user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
@@ -45,7 +46,12 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::withCount('posts')
+            ->withCount('comments')
+            ->withCount('likes_posts')
+            ->findOrFail($id);
+
+        return view('userdetail',compact('user'));
     }
 
     /**
