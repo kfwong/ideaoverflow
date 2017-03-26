abcc03baea604f0d07ccad12647798dc4637bac2
Fang Ruoming <frm10802@hotmail.com>
2017-03-27 05:28:55 +0800

    improves form style, implements model binding

--- resources/views/userprofile/usereditform.blade.php
+++ resources/views/userprofile/usereditform.blade.php
@@ -8,20 +8,45 @@
 
 <div>
 
-{!! Form::open() !!}
+{!! Form::model($user,array('route'=>array('users.update',$user->id),'method'=>'PUT')) !!}
 
-<div>
+<div class="form-group">
 
-{!! Form::label('Name') !!}
+{!! Form::label('name','Name') !!}
 <br>
-{!! Form::text('name',null) !!}
+{!! Form::text('name',null,array('class'=>'form-control')) !!}
 
 </div>
 
-<div>
+<div class="form-group">
+	
+	{!! Form::label('username','Username') !!}
+	<br>
+	{!! Form::text('username',null,array('class'=>'form-control')) !!}
 
-{!! Form::submit('Update Profile', 
-	array('class'=>'btn btn-primary')) !!}
+</div>
+
+<div class="form-group">
+	
+	{!! Form::label('email','Email') !!}
+	<br>
+	{!! Form::text('email',null,array('class'=>'form-control')) !!}
+
+</div>
+
+<div class="form-group">
+	
+	{!! Form::label('description','Description') !!}
+	<br>
+	{!! Form::textarea('description',null,array('class'=>'form-control')) !!}
+	
+</div>
+
+<div class="form-group">
+
+	<br>
+	{!! Form::submit('Update Profile', 
+		array('class'=>'btn btn-primary')) !!}
 
 </div>
 
