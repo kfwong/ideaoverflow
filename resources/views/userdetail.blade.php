09ba717d9b3c0e8cc2c1bb303a9a7d4667f76759
kfwong <kfwong@zoho.com>
2017-04-03 11:47:49 +0800

    Fix the alignment again.

--- resources/views/userdetail.blade.php
+++ resources/views/userdetail.blade.php
@@ -14,26 +14,31 @@
 
 @section('content')
 
-@if(Session::has('message'))
-<div class="alert alert-success alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  {{ Session::get('message') }}
-</div>        
-@endif
+    <div id="content" class="row">
+        <main class="col-xs-12 col-md-offset-1 col-md-10">
+            @if(Session::has('message'))
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                    {{ Session::get('message') }}
+                </div>
+            @endif
 
-<div class="row">
-    <div class = "col-xs-3" >
-        @include('userprofile.usermaster')
-        @can('update', $user)
-        <div>
-            <a class="btn btn-primary" href="/users/{{$user->id}}/edit">Edit Profile</a>
-        </div>
-        @endcan
-    </div>
+            <div class="row">
+                <div class="col-xs-3">
+                    @include('userprofile.usermaster')
+                    @can('update', $user)
+                    <div>
+                        <a class="btn btn-primary" href="/users/{{$user->id}}/edit">Edit Profile</a>
+                    </div>
+                    @endcan
+                </div>
 
-    <div class = "col-xs-9" >
-        @include('userprofile.usercontents')
+                <div class="col-xs-9">
+                    @include('userprofile.usercontents')
+                </div>
+            </div>
+        </main>
     </div>
-</div>
 
 @endsection
