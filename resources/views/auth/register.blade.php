39faeb10cf35d9b8b9febf8db4fa05490dc5d673
kfwong <kfwong@zoho.com>
2017-04-05 17:10:38 +0800

    Fix gender registration causes error. malform controller attribute.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,9 +5,8 @@
 @section('content-title', 'Ideaoverflow')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div id="content" class="row">
+        <main class="col-xs-12 col-md-offset-4 col-md-4 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-heading">Ideaoverflow Sign Up</div>
                 <div class="panel-body">
@@ -18,7 +17,8 @@
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
+                                       required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -27,12 +27,13 @@
                                 @endif
                             </div>
                         </div>
-												
+
                         <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
                             <label for="username" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+                                <input id="username" type="text" class="form-control" name="username"
+                                       value="{{ old('username') }}" required autofocus>
 
                                 @if ($errors->has('username'))
                                     <span class="help-block">
@@ -46,7 +47,8 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email"
+                                       value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -55,15 +57,23 @@
                                 @endif
                             </div>
                         </div>
-						
-						<div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
+
+                        <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">Gender</label>
 
                             <div class="col-md-6">
-								<input id="gender" type="radio" class="form-control" name="gender" value="not specified" required><label for="not specified"> not specified</label><br>
-								<input id="gender" type="radio" class="form-control" name="gender" value="male" required><label for="male"> male</label><br>
-								<input id="gender" type="radio" class="form-control" name="gender" value="female" required><label for="female"> female</label><br>
-                                
+                                <label class="radio-inline">
+                                    <input id="gender" type="radio" name="gender" value="not specified" required><label
+                                            for="not specified"> not specified</label>
+                                </label>
+                                <label class="radio-inline">
+                                    <input id="gender" type="radio" name="gender" value="female" required><label
+                                            for="female"> female</label>
+                                </label>
+                                <label class="radio-inline">
+                                    <input id="gender" type="radio" name="gender" value="male" required><label
+                                            for="male"> male</label>
+                                </label>
 
                                 @if ($errors->has('gender'))
                                     <span class="help-block">
@@ -91,7 +101,8 @@
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" class="form-control"
+                                       name="password_confirmation" required>
                             </div>
                         </div>
 
@@ -105,7 +116,6 @@
                     </form>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
-</div>
 @endsection
