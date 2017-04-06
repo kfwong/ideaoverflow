eace7cfa27d3140e4cf6362c3a666d558ee7e5c2
ElodieIkkache <elodie.ikkache@student.ecp.fr>
2017-04-06 21:43:48 +0800

    adding the white version of the logo on the head banner

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -2,13 +2,16 @@
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
+      <img src = "img/logoblanc.png" width = "65px" text_align = "center">
+	</div>
+    <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Ideaoverflow</a>
+      <a class="navbar-brand" href="/"> Ideaoverflow </a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right text-center">
