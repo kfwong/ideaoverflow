0457f55838b93373b2a9320ccc375232ab3da7c1
kfwong <kfwong@zoho.com>
2017-04-07 07:57:59 +0000

    Fix unsanitized input.

--- resources/assets/js/search.js
+++ resources/assets/js/search.js
@@ -1,3 +1,20 @@
+var entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
+
+function escapeHtml (string) {
+  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+    return entityMap[s];
+  });
+}
+
 $(document).on('click', function () {
     $('.results').hide();
 });
@@ -20,7 +37,7 @@ $('#searchbox').on('keyup', function () {
                 if (data.length > 0) {
                     data.forEach(function (item) {
                         var dateUpdated = moment(item.updated_at, 'YYYY-MM-DD HH:mm:ss').fromNow();
-                        $('.results').append('<li><a href="/posts/' + item.id + '"><span class="result-title">' + item.title + '</span>&nbsp;&nbsp;<span class="label tag-'+item.type+'">'+item.type+'</span>&nbsp;&nbsp;<small class="result-time"><i class="fa fa-clock-o"></i>&nbsp;'+dateUpdated+'</small><br/><span class="result-body">' + item.body + '</span></a></li>');
+                        $('.results').append('<li><a href="/posts/' + item.id + '"><span class="result-title">' + escapeHtml(item.title) + '</span>&nbsp;&nbsp;<span class="label tag-'+item.type+'">'+escapeHtml(item.type)+'</span>&nbsp;&nbsp;<small class="result-time"><i class="fa fa-clock-o"></i>&nbsp;'+dateUpdated+'</small><br/><span class="result-body">' + escapeHtml(item.body) + '</span></a></li>');
                     });
                 } else {
                     $('.results').append('<li><a href="#">No result found... Try something else?</a></li>');
